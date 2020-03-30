08bf28d7f54de85b22346938d58fd3946b2a3a3e
gittourist <6751325@gmail.com>
2020-03-30 17:25:08 +0500

    after refactoring calc-game3

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,5 +1,7 @@
 import { runBrainGame, totalCounter } from '../index';
-import { getRandInt, cons } from '../utils';
+import {
+  getRandInt, cons, car, cdr,
+} from '../utils';
 
 const getAnswer = (num1, num2, operator) => {
   let expressionResult = num1 * num2;
@@ -14,22 +16,26 @@ const getAnswer = (num1, num2, operator) => {
   return expressionResult;
 };
 
-const getArrPairQuestAndAnsw = (count, arrPair = []) => {
-  if (count === 0) {
-    return arrPair;
+const getOperator = (oper = ['-', '+', '*']) => oper[getRandInt(2)];
+
+const getPairArrQuestAndAnswer = (counter) => {
+  const arrayQuestion = [];
+  const arrayAnswer = [];
+
+  for (let i = 0; i < counter; i += 1) {
+    const number1 = getRandInt(99);
+    const number2 = getRandInt(99);
+    const operator = getOperator();
+
+    arrayQuestion.push(`${number1} ${operator} ${number2}`);
+    arrayAnswer.push(String(getAnswer(number1, number2, operator)));
   }
-  const getOperator = (oper = ['-', '+', '*']) => oper[getRandInt(2)];
-  const number1 = getRandInt(99);
-  const number2 = getRandInt(99);
-  const operator = getOperator();
-
-  const question = `${number1} ${operator} ${number2}`;
-  const answer = String(getAnswer(number1, number2, operator));
-  // console.log(`answer ${String(answer)}`);
-  arrPair.push(cons(question, answer));
-
-  return getArrPairQuestAndAnsw(count - 1, arrPair);
+  return cons(arrayQuestion, arrayAnswer);
 };
 
+const pairArrQuestAndAnswer = getPairArrQuestAndAnswer(totalCounter);
+const question = car(pairArrQuestAndAnswer);
+const answer = cdr(pairArrQuestAndAnswer);
 const rule = 'What is the result of the expression?';
-export default () => runBrainGame(rule, getArrPairQuestAndAnsw(totalCounter));
+
+export default () => runBrainGame(rule, question, answer);
